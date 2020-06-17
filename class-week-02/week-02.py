8bfcd3cee09e38b2b0750cad8c25f8efb4b621fd
fuchs <fuchs_leo@hotmail.com>
2020-06-17 14:04:28 -0400

    Updating Class Items from Week 02

--- class-week-02/week-02.py
+++ class-week-02/week-02.py
@@ -17,41 +17,51 @@ def get_table_download_link(df):
 def main():
     st.image('logo.png', width=200)
     st.title('AceleraDev Data Science')
-    st.header('Teste')
-    st.subheader('Semana 2 - Pré-processamento de Dados em Python')
-    st.image('https://media.giphy.com/media/KyBX9ektgXWve/giphy.gif', width=200)
+    st.header('Semana 2 - Pré-Processamento de Dados em Python')
+
     file = st.file_uploader('Escolha a base de dados que deseja analisar (.csv)', type='csv')
     if file is not None:
-        st.subheader('Analisando os dados')
+        st.subheader('Analisando os dados do dataframe (5 primeiras linhas):')
         df = pd.read_csv(file)
         st.dataframe(df.head(5))
-        st.markdown('**Número de linhas:**')
+
+        st.markdown('**Número total de linhas:**')
         st.markdown(df.shape[0])
-        st.markdown('**Número de colunas:**')
+
+        st.markdown('**Número total de colunas:**')
         st.markdown(df.shape[1])
-        st.markdown('**Visualizando o dataframe**')
-        number = st.slider('Escolha o numero de colunas que deseja ver', min_value=1, max_value=20)
+
+        st.markdown('**Visualização personalizada do dataframe:**')
+        number = st.slider('Escolha o numero de colunas que deseja ver:', min_value=1, max_value=10)
         st.dataframe(df.head(number))
+
         st.markdown('**Nome das colunas:**')
         st.markdown(list(df.columns))
+
         exploracao = pd.DataFrame({'nomes': df.columns, 'tipos': df.dtypes, 'NA #': df.isna().sum(),
                                    'NA %': (df.isna().sum() / df.shape[0]) * 100})
+
         st.markdown('**Contagem dos tipos de dados:**')
         st.write(exploracao.tipos.value_counts())
+
         st.markdown('**Nomes das colunas do tipo int64:**')
         st.markdown(list(exploracao[exploracao['tipos'] == 'int64']['nomes']))
+
         st.markdown('**Nomes das colunas do tipo float64:**')
         st.markdown(list(exploracao[exploracao['tipos'] == 'float64']['nomes']))
+
         st.markdown('**Nomes das colunas do tipo object:**')
         st.markdown(list(exploracao[exploracao['tipos'] == 'object']['nomes']))
-        st.markdown('**Tabela com coluna e percentual de dados faltantes :**')
+
+        st.markdown('**Tabela com coluna e percentual de dados faltantes:**')
         st.table(exploracao[exploracao['NA #'] != 0][['tipos', 'NA %']])
-        st.subheader('Inputaçao de dados númericos :')
-        percentual = st.slider(
-            'Escolha o limite de percentual faltante limite para as colunas vocë deseja inputar os dados', min_value=0,
-            max_value=100)
+
+        st.subheader('Inputaçao de dados numéricos :')
+        percentual = st.slider('Limite de percentual faltante para as colunas você deseja inputar os dados',
+                               min_value=0, max_value=100)
         lista_colunas = list(exploracao[exploracao['NA %'] < percentual]['nomes'])
-        select_method = st.radio('Escolha um metodo abaixo :', ('Média', 'Mediana'))
+
+        select_method = st.radio('Escolha um método abaixo:', ('Média', 'Mediana'))
         st.markdown('Você selecionou : ' + str(select_method))
         if select_method == 'Média':
             df_inputado = df[lista_colunas].fillna(df[lista_colunas].mean())
@@ -59,15 +69,18 @@ def main():
                 {'nomes': df_inputado.columns, 'tipos': df_inputado.dtypes, 'NA #': df_inputado.isna().sum(),
                  'NA %': (df_inputado.isna().sum() / df_inputado.shape[0]) * 100})
             st.table(exploracao_inputado[exploracao_inputado['tipos'] != 'object']['NA %'])
-            st.subheader('Dados Inputados faça download abaixo : ')
+
+            st.subheader('Dados inputados disponíveis (faça o download abaixo): ')
             st.markdown(get_table_download_link(df_inputado), unsafe_allow_html=True)
+
         if select_method == 'Mediana':
             df_inputado = df[lista_colunas].fillna(df[lista_colunas].median())
             exploracao_inputado = pd.DataFrame(
                 {'nomes': df_inputado.columns, 'tipos': df_inputado.dtypes, 'NA #': df_inputado.isna().sum(),
                  'NA %': (df_inputado.isna().sum() / df_inputado.shape[0]) * 100})
             st.table(exploracao_inputado[exploracao_inputado['tipos'] != 'object']['NA %'])
-            st.subheader('Dados Inputados faça download abaixo : ')
+
+            st.subheader('Dados inputados disponíveis (faça download abaixo): ')
             st.markdown(get_table_download_link(df_inputado), unsafe_allow_html=True)
 
 
